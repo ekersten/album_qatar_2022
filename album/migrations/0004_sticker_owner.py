f7892c0a62c0309dcc55f28ae747d3ea9edbaf85
Eric Kersten <ekersten@gmail.com>
2022-09-18 09:01:03 -0300

    cambio migracion

--- album/migrations/0004_sticker_owner.py
+++ album/migrations/0004_sticker_owner.py
@@ -16,7 +16,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='sticker',
             name='owner',
-            field=models.ForeignKey(default=2, on_delete=django.db.models.deletion.CASCADE, related_name='stickers', to=settings.AUTH_USER_MODEL),
+            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='stickers', to=settings.AUTH_USER_MODEL),
             preserve_default=False,
         ),
     ]
